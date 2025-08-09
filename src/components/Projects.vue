a055f70b1767237155334da32d04fa2f4d5acc19
Perky98 <jurajhertel98@gmail.com>
2025-08-09 23:51:23 +0200

    new: projects info

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -6,24 +6,74 @@
   <section class="mx-auto w-full max-w-3xl  ">
       <h2 class="text-center md:text-left  text-3xl font-bold my-8">Projects</h2>
     <div class="grid gap-8 grid-cols-1 md:grid-cols-2">
-      <div class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl mx-4 md:mx-0 px-8 pb-8 pt-40 ">
+      <a href="https://www.finpomb.sk/" target="_blank" class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl mx-4 md:mx-0 px-8 pb-8 pt-40 ">
         <img src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a" alt="University of Southern California" class="absolute inset-0 h-full w-full object-cover">
         <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-        <h3 class="z-10 mt-3 text-3xl font-bold text-white">Paris</h3>
-        <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">City of love</div>
-      </div>
-      <div class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl mx-4 md:mx-0 px-8 pb-8 pt-40  ">
+        <h3 class="z-10 mt-3 text-3xl font-bold text-white">FinpoMB</h3>
+        <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">PHP, SCSS, Wordpress, Javascript</div>
+      </a>
+      <a href="https://www.parkingaround.com/" target="_blank"  class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl mx-4 md:mx-0 px-8 pb-8 pt-40  ">
         <img src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a" alt="University of Southern California" class="absolute inset-0 h-full w-full object-cover">
         <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-        <h3 class="z-10 mt-3 text-3xl font-bold text-white">Paris</h3>
-        <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">City of love</div>
-      </div>
-      <div class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl mx-4 md:mx-0 px-8 pb-8 pt-40">
+        <h3 class="z-10 mt-3 text-3xl font-bold text-white">ParkingAround</h3>
+        <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">PHP, Laravel, Alpine, Tailwind, VueJS</div>
+      </a>
+      <a href="https://www.bazeny-viking.sk/" target="_blank"  class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl mx-4 md:mx-0 px-8 pb-8 pt-40">
         <img src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a" alt="University of Southern California" class="absolute inset-0 h-full w-full object-cover">
         <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-        <h3 class="z-10 mt-3 text-3xl font-bold text-white">Paris</h3>
-        <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">City of love</div>
-      </div>
+        <h3 class="z-10 mt-3 text-3xl font-bold text-white">Bazeny Viking</h3>
+        <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">PHP, SCSS, Wordpress, Javascript</div>
+      </a>
+      <a href="https://www.tekoldany.sk/" target="_blank"  class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl mx-4 md:mx-0 px-8 pb-8 pt-40">
+        <img src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a" alt="University of Southern California" class="absolute inset-0 h-full w-full object-cover">
+        <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+        <h3 class="z-10 mt-3 text-3xl font-bold text-white">Tekoldany</h3>
+        <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">Wordpress Elementor, SCSS</div>
+      </a>
+      <a href="https://www.bielakocka.sk/" target="_blank"  class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl mx-4 md:mx-0 px-8 pb-8 pt-40">
+        <img src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a" alt="University of Southern California" class="absolute inset-0 h-full w-full object-cover">
+        <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+        <h3 class="z-10 mt-3 text-3xl font-bold text-white">Biela kocka</h3>
+        <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">Wordpress Elementor</div>
+      </a>
+      <a href="https://aikido-suchdol.cz/" target="_blank"  class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl mx-4 md:mx-0 px-8 pb-8 pt-40">
+        <img src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a" alt="University of Southern California" class="absolute inset-0 h-full w-full object-cover">
+        <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+        <h3 class="z-10 mt-3 text-3xl font-bold text-white">Aikido suchodol</h3>
+        <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">Wordpress Elementor</div>
+      </a>
+      <a href="https://www.goldeneagle.sk/" target="_blank"  class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl mx-4 md:mx-0 px-8 pb-8 pt-40">
+        <img src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a" alt="University of Southern California" class="absolute inset-0 h-full w-full object-cover">
+        <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+        <h3 class="z-10 mt-3 text-3xl font-bold text-white">Hotel Golden Eagle</h3>
+        <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">Wordpress Elementor, SCSS</div>
+      </a>
+      <a href="https://www.profipotrubie.sk/" target="_blank"  class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl mx-4 md:mx-0 px-8 pb-8 pt-40">
+        <img src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a" alt="University of Southern California" class="absolute inset-0 h-full w-full object-cover">
+        <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+        <h3 class="z-10 mt-3 text-3xl font-bold text-white">Profipotrubie</h3>
+        <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">Wordpress Elementor, SCSS</div>
+      </a>
+
+      <a href="https://www.georadar.sk/" target="_blank"  class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl mx-4 md:mx-0 px-8 pb-8 pt-40">
+        <img src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a" alt="University of Southern California" class="absolute inset-0 h-full w-full object-cover">
+        <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+        <h3 class="z-10 mt-3 text-3xl font-bold text-white">GeoRadar</h3>
+        <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">Wordpress Elementor</div>
+      </a>
+      <a href="https://www.krtkofix.sk/" target="_blank"  class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl mx-4 md:mx-0 px-8 pb-8 pt-40">
+        <img src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a" alt="University of Southern California" class="absolute inset-0 h-full w-full object-cover">
+        <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+        <h3 class="z-10 mt-3 text-3xl font-bold text-white">Krtkofix</h3>
+        <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">Wordpress Elementor</div>
+      </a>
+      <a href="https://www.strechybartko.sk/" target="_blank"  class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl mx-4 md:mx-0 px-8 pb-8 pt-40">
+        <img src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a" alt="University of Southern California" class="absolute inset-0 h-full w-full object-cover">
+        <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+        <h3 class="z-10 mt-3 text-3xl font-bold text-white">Strechy Bartko</h3>
+        <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">Wordpress Elementor, SCSS</div>
+      </a>
+
     </div>
 
   </section>
