029b66e22ded9c0c33ce51b530c8735ca9b114f3
Perky98 <jurajhertel98@gmail.com>
2025-08-10 18:07:05 +0200

    new: images of projects

--- src/components/Projects/Myweb.vue
+++ src/components/Projects/Myweb.vue
@@ -14,8 +14,8 @@ const props = defineProps<{
     class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl mx-4 md:mx-0 px-8 pb-8 pt-40"
   >
     <img
-      src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a"
-      alt="University of Southern California"
+      :src="`/src/assets/projects/${image}.png`"
+      alt="{{ name }}"
       class="absolute inset-0 h-full w-full object-cover"
     />
     <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
