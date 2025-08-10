eac1393513dd5e0bb5fa4176fc640c3c4c2354f0
Perky98 <jurajhertel98@gmail.com>
2025-08-10 15:22:57 +0200

    fix: wrong tag

--- src/components/Experience/Edu.vue
+++ src/components/Experience/Edu.vue
@@ -12,7 +12,7 @@ const props = defineProps<{
     <img class="w-[30px] h-[30px] mt-1" :src="`/src/assets/${image}`" alt="{{name}}" />
     <div>
       <h2 class="text-2xl font-bold text-gray-100">{{ title }}</h2>
-      <spana class="font-bold text-gray-100 text-lg" >{{name}}</spana>
+      <span class="font-bold text-gray-100 text-lg" >{{name}}</span>
       <h3 class="text-gray-100">{{ durations }}</h3>
     </div>
   </div>
