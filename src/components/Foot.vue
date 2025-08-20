21d6e1eda7fd6f10876b0fa9a5283526f5090872
Perky98 <jurajhertel98@gmail.com>
2025-08-20 20:21:01 +0200

    edit: footer

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -7,7 +7,7 @@
     <div class="mx-auto max-w-5xl overflow-hidden px-6 py-10 lg:px-8">
 
 
-      <p class="mt-10 text-center text-sm/6 text-gray-600 dark:text-gray-400">&copy; 2025 Juraj Hertel, Inc. All rights reserved.</p>
+      <p class="mt-10 text-center text-sm/6 text-gray-600 dark:text-gray-400">&copy; 2025 Juraj Hertel, All rights reserved.</p>
     </div>
   </footer>
 </template>
